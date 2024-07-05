0e353ba0e001dbec76dc3e3df1b0ae2595d233e8
SudheerPedagadi <pedagadivenkat083@gmail.com>
2024-07-05 17:22:06 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,41 +1,76 @@
 #include <stdio.h>
 #include <assert.h>
 
-int isTemperatureOk(float temperature) {
-    if (temperature < 0 || temperature > 45) {
-        printf("Temperature out of range!\n");
-        return 0;
-    }
-    return 1;
-}
-
-int isSocOk(float soc) {
-    if (soc < 20 || soc > 80) {
-        printf("State of Charge out of range!\n");
-        return 0;
-    }
-    return 1;
-}
-
-int isChargeRateOk(float chargeRate) {
-    if (chargeRate > 0.8) 
+// Function to print error message and return 0
+int checkAndPrintError(int condition, const char *errorMessage) 
+{
+    if (!condition)
     {
-        printf("Charge Rate out of range!\n");
-        return 0;
+      printf("%s\n", errorMessage);
+      return 0;
     }
     return 1;
 }
 
-int batteryIsOk(float temperature, float soc, float chargeRate) 
+int isTemperatureOk(float temperature)
 {
-    int tempOk = isTemperatureOk(temperature);
-    int socOk = isSocOk(soc);
-    int chargeRateOk = isChargeRateOk(chargeRate);
-    return tempOk && socOk && chargeRateOk;
+  return checkAndPrintError(temperature >= 0 && temperature <= 45, "Temperature out of range!");
 }
 
-int main() 
+int isSocOk(float soc) 
 {
- assert(batteryIsOk(25, 70, 0.7));
- assert(!batteryIsOk(50, 85, 0));
+  return checkAndPrintError(soc >= 20 && soc <= 80, "State of Charge out of range!");
 }
+
+int isChargeRateOk(float chargeRate)
+{
+  return checkAndPrintError(chargeRate <= 0.8, "Charge Rate out of range!");
+}
+
+int batteryIsOk(float temperature, float soc, float chargeRate)
+{
+  int tempOk = isTemperatureOk(temperature);
+  int socOk = isSocOk(soc);
+  int chargeRateOk = isChargeRateOk(chargeRate);
+  return tempOk && socOk && chargeRateOk;
+}
+
+// Struct to hold test case data
+typedef struct
+{
+  float temperature;
+  float soc;
+  float chargeRate;
+} TestCase;
+
+// Array of test cases
+TestCase testCases[] = 
+{
+  { 25, 70, 0.7 },
+  { 0, 20, 0.8 },
+  { 45, 80, 0.8 },
+  { 50, 85, 0 },      
+  { -5, 70, 0.7 }, 
+  { 25, 15, 0.7 },   
+  { 25, 85, 0.7 },   
+  { 25, 70, 0.85 },   
+  { 0, 70, 0.7 },    
+  { 25, 19, 0.7 },   
+  { 25, 70, 0.81 },   
+  { 0, 0, 0 },        
+  { 25, 70, -0.5 }, 
+};
+
+int main()
+{
+  size_t numTestCases = sizeof(testCases) / sizeof(testCases[0]);
+  for (size_t i = 0; i < numTestCases; ++i)
+  {
+    float temperature = testCases[i].temperature;
+    float soc = testCases[i].soc;
+    float chargeRate = testCases[i].chargeRate;
+    assert(batteryIsOk(temperature, soc, chargeRate));
+    assert(!batteryIsOk(temperature, soc, chargeRate));
+  }
+}
+
